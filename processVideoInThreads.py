3a8cf2326e2e3b6161f53dc891cf745023717320
Ing. Klaus Schlender <info@itlerhilfe.de>
2023-03-10 02:12:56 +0100

    ADD: progress bar shows the overall processing

--- processVideoInThreads.py
+++ processVideoInThreads.py
@@ -6,7 +6,6 @@ from moviepy.video.VideoClip import ColorClip
 from moviepy.audio.AudioClip import CompositeAudioClip
 from moviepy.video.compositing.concatenate import concatenate_videoclips
 
-
 # function to process a video chunk
 def process_chunk(chunk_idx):
     # load video chunk
@@ -42,6 +41,13 @@ def process_chunk(chunk_idx):
     output_filename = f"output_{chunk_idx:02d}{os.path.splitext(input_file)[1]}"
     video_clip.write_videofile(output_filename, audio_codec='aac')
 
+    # update progress bar
+    with lock:
+        global num_processed_chunks
+        num_processed_chunks += 1
+        progress = num_processed_chunks / num_chunks
+        print(f"Processed {num_processed_chunks}/{num_chunks} chunks ({progress:.1%})")
+
 # function to process audio clip
 def process_audio_clip(audio_clip):
     # convert audio from 440hz to 432hz
@@ -60,6 +66,10 @@ chunk_duration = 5
 # number of chunks to split the video into
 num_chunks = 45
 
+# initialize progress bar variables
+num_processed_chunks = 0
+lock = threading.Lock()
+
 # split the video into chunks and process each chunk in a separate thread
 threads = []
 for i in range(num_chunks):
@@ -76,5 +86,4 @@ output_files = sorted([f for f in os.listdir() if f.startswith("output_")])
 clips = [VideoFileClip(f) for f in output_files]
 video_clip = concatenate_videoclips(clips)
 audio_clip = CompositeAudioClip([clip.audio for clip in clips])
-video_clip = video_clip.set_audio(audio_clip)
-video_clip.write_videofile("432hzVideoOutput.mp4", audio_codec='aac')
+video_clip = video_clip.set_audio
