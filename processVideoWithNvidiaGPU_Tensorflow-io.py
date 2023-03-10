6e6da8874a8a0c93ce43ea563375c491cdefd74b
Ing. Klaus Schlender <info@itlerhilfe.de>
2023-03-10 02:38:52 +0100

    ADD: Video GPU processing Tensorflow-IO python

---
+++ processVideoWithNvidiaGPU_Tensorflow-io.py
@@ -0,0 +1,36 @@
+"""
+This code uses tfio.IOTensor to read the input video file and extract the audio and video streams. It then uses tfio.audio.resample to resample the audio stream to 432 Hz. Next, it uses tfio.audio.rms and tfio.audio.dbscale to compute the audio level of each segment of the video. Based on the audio level, it speeds up the video using segment.video.speed for segments where the audio level is low. Finally, it concatenates the segments back together using tfio.experimental.vision.concatenate, and writes the output video file using tfio.IOTensor.to_ffmpeg.
+
+Note that in order to use GPU acceleration with tensorflow-io, you'll need to have a compatible version of TensorFlow installed with GPU support. You can check if GPU support is available by running tf.config.list_physical_devices('GPU'), which should return a list of detected GPUs if everything is set up correctly.
+"""
+import tensorflow_io as tfio
+
+# Define the input and output video files
+input_file = "videoInput.mp4"
+output_file = "432hzVideoOutput.mp4"
+
+# Open the input video file
+input_video = tfio.IOTensor.from_ffmpeg(input_file)
+
+# Extract the audio stream and resample it to 432 Hz
+audio_stream = input_video[0]
+audio_stream = tfio.audio.resample(audio_stream, rate_in=audio_stream.rate, rate_out=432)
+
+# Extract the video stream and split it into segments based on audio level
+video_stream = input_video[1]
+audio_levels = tfio.audio.dbscale(tfio.audio.rms(audio_stream), top_db=30)
+segments = tfio.experimental.vision.split(video_stream, audio_levels)
+
+# Speed up the video in segments where audio level is low
+speed_up_segments = []
+for segment in segments:
+    audio_level = tf.reduce_mean(tfio.audio.dbscale(tfio.audio.rms(segment.audio), top_db=30))
+    if audio_level < -50:
+        speed_up_segments.append(segment.video.speed(4.0))
+
+# Concatenate the segments back together
+final_video = tfio.experimental.vision.concatenate(speed_up_segments)
+
+# Write the output video file
+tfio.IOTensor.to_ffmpeg(final_video, output_file)
+
