4cf48f9b01d774281de5644f10c1d9604615008c
Ing. Klaus Schlender <info@itlerhilfe.de>
2023-03-10 02:41:44 +0100

    Update: codebase to tensorflow memory usage

--- processVideoWithNvidiaGPU_Tensorflow-io.py
+++ processVideoWithNvidiaGPU_Tensorflow-io.py
@@ -2,12 +2,19 @@
 This code uses tfio.IOTensor to read the input video file and extract the audio and video streams. It then uses tfio.audio.resample to resample the audio stream to 432 Hz. Next, it uses tfio.audio.rms and tfio.audio.dbscale to compute the audio level of each segment of the video. Based on the audio level, it speeds up the video using segment.video.speed for segments where the audio level is low. Finally, it concatenates the segments back together using tfio.experimental.vision.concatenate, and writes the output video file using tfio.IOTensor.to_ffmpeg.
 
 Note that in order to use GPU acceleration with tensorflow-io, you'll need to have a compatible version of TensorFlow installed with GPU support. You can check if GPU support is available by running tf.config.list_physical_devices('GPU'), which should return a list of detected GPUs if everything is set up correctly.
+
+In this version of the code, I've added an import statement for tensorflow and configured it to use the GPU using tf.config.experimental.set_memory_growth. The rest of the code is the same as the previous version, using tfio.IOTensor to read the input video file, and various tfio functions for audio resampling, video splitting and concatenation.
 """
+import tensorflow as tf
 import tensorflow_io as tfio
 
 # Define the input and output video files
 input_file = "videoInput.mp4"
 output_file = "432hzVideoOutput.mp4"
+
+# Configure TensorFlow to use GPU
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 # Open the input video file
 input_video = tfio.IOTensor.from_ffmpeg(input_file)
@@ -33,4 +40,5 @@ final_video = tfio.experimental.vision.concatenate(speed_up_segments)
 
 # Write the output video file
 tfio.IOTensor.to_ffmpeg(final_video, output_file)
+
 
